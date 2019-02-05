a1ecd341e5584f6d958ee99e8abc979838a770e5
Jerry Li <xiaoroyg@163.com>
2019-02-05 11:27:01 +0800

    Learned Enumerations and Structures

--- SwiftTourSecondPlayground.playground/Contents.swift
+++ SwiftTourSecondPlayground.playground/Contents.swift
@@ -90,3 +90,98 @@ class TriangleAndSquare {
         square = Square(sideLength: size, name: name)
     }
 }
+
+//Enumerations and Structures
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        var result: String
+        switch self {
+        case .ace:
+            result = "ace"
+            break
+        case .jack:
+            result = "jack"
+            break
+        case .queen:
+            result = "queen"
+            break
+        case .king:
+            result = "kind"
+            break
+        default:
+            result = String(self.rawValue)
+        }
+        return result
+    }
+    
+    static let allVaues = [ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king]
+}
+
+if let convertedRank = Rank(rawValue: 3) {
+    convertedRank.simpleDescription()
+}
+
+enum Suit {
+    case spades, hearts, dimonds, clubs
+    
+    func color() -> String {
+        var result: String
+        switch self {
+        case .spades, .clubs:
+            result = "black"
+        default:
+            result = "red"
+        }
+        return result
+    }
+    
+    static let allValues = [spades, hearts, dimonds, clubs]
+}
+
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+    case noResponse(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out of cheers")
+let noResponse = ServerResponse.noResponse("Server is Down")
+
+switch success {
+case let .result(sunrise, sunset) :
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset)")
+case let .failure(messge) :
+    print("Failure... \(messge)")
+case let .noResponse(error) :
+    print(error)
+}
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit)"
+    }
+}
+
+let threeOfSpades = Card(rank: .three, suit: .spades)
+print(threeOfSpades.simpleDescription())
+
+func fullCards() -> [Card] {
+    var result = [Card]()
+    for rank in Rank.allVaues {
+        for suit in Suit.allValues {
+            result.append(Card(rank: rank, suit: suit))
+        }
+    }
+    return result
+}
+
+print(fullCards().count)
