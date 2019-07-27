6887593c99ec39fc49c7d8c2193d8b9d4d6e1757
Jerry Li <xiaoroyg@163.com>
2019-07-27 23:07:04 +0800

    Learned name space in swift

---
+++ iOS12ProgrammingFundamentalsWithSwift/ChapterOne.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+let sum = 1 + 2
+
+let sumDescription = sum.description
+
+extension Int {
+    func sayHello() {
+        print("Hello, I'm \(self)")
+    }
+}
+
+sum.sayHello()
+
+class Student {
+    func showBook() {
+        print(Book())
+    }
+    
+    func showSum() {
+        print(sum)
+    }
+    
+    func inside() {
+        print(Inside())
+    }
+    
+    class Inside{
+    
+    }
+}
+
+class Book {
+    class Inside {
+        func what() {
+            let what = Student.Inside()
+            print(what)
+        }
+    }
+}
