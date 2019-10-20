1d44369cd7073f53672e34f34882133316dda1d4
Jerry Li <xiaoroyg@163.com>
2019-10-20 22:33:25 +0800

    Learned computed variable.

--- iOS12ProgrammingFundamentalsWithSwift/ChapterThree.playground/Contents.swift
+++ iOS12ProgrammingFundamentalsWithSwift/ChapterThree.playground/Contents.swift
@@ -52,3 +52,93 @@ var taskIdentifierC = TaskIdentifier()
 taskIdentifierC = beginTask {
     endTask(taskIdentifierC)
 }
+
+//Computed Initializer
+let value = {
+    3
+}()
+
+let shouldGo: Bool =  {
+    if value == 1 {
+        return true
+    } else {
+        return false
+    }
+}()
+
+class Author {
+    var name: String = ""
+    var country: String = ""
+}
+
+class Info {
+    var pageCount: Int = 0
+    var price: Double = 0.0
+}
+
+class Book {
+    let author: Author = {
+        () -> Author in
+        let author: Author = Author()
+        author.name = "JK"
+        author.country = "UK"
+        return author
+    }()
+    
+    
+    //let info: Info = self.findInfo()  it is not legal to declare an instance method and initialize the instance property by calling that method
+    
+    func findInfo() -> Info {
+        return Info()
+    }
+}
+
+//Computed Variables
+
+var now: String {
+    get {
+        return Date().description
+    }
+    set {
+        print(newValue)
+    }
+}
+
+now = "not now"
+print(now)
+
+var anotherNow: String {
+    get {
+        return Date().description
+    }
+    set(anotherValue) {
+        print(anotherValue)
+    }
+}
+
+var nowOnlyGetter: String {
+    return Date().description
+}
+
+class Data {}
+
+class BackgroundHandler {
+    
+    private var _cachedData: Data! = nil
+    
+    var cachedData: Data  {
+        set (newData) {
+            self._cachedData = newData
+        }
+        
+        get {
+            if _cachedData == nil {
+                // get from the disk
+                self._cachedData = Data() // mock get from the data
+            }
+            return self._cachedData
+        }
+    }
+    
+    
+}
