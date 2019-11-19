3f29bb22d17742d6e4de3264fc2662a69437cad7
Jerry Li <xiaoroyg@163.com>
2019-11-19 22:38:12 +0800

    Learned enum initializer.

--- iOS12ProgrammingFundamentalsWithSwift/ChapterFourB.playground/Contents.swift
+++ iOS12ProgrammingFundamentalsWithSwift/ChapterFourB.playground/Contents.swift
@@ -25,4 +25,24 @@ enum StoryState: Int {
 }
 print(StoryState.unStarted)
 
+enum WhatError {
+    case number(Int)
+    case message(String)
+    case fatal(code: Int, message: String)
+}
+
+let errorA = WhatError.number(4)
+let errorB = WhatError.fatal(code: 12, message: "fatal One")
+
+enum FilterB: String, CaseIterable {
+    case albums = "Albums"
+    case playlists = "Playlists"
+    case podcasts = "Podcasts"
+    case books = "Books"
+    
+    init(_ index: Int) {
+        self = FilterB.allCases[index]
+    }
+}
+let filters = FilterB.allCases
 
