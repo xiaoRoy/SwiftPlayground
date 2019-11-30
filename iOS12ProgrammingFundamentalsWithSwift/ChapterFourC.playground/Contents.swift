416c78b477c2d41de34247b07bf55e2579fdd50a
Jerry Li <xiaoroyg@163.com>
2019-11-30 17:02:56 +0800

    Override the super class initializer.

--- iOS12ProgrammingFundamentalsWithSwift/ChapterFourC.playground/Contents.swift
+++ iOS12ProgrammingFundamentalsWithSwift/ChapterFourC.playground/Contents.swift
@@ -100,7 +100,7 @@ class Car {
 //    }
 }
 
-//example shows that subclass only has convenience initializer
+
 
 class Book {
     let id: String
@@ -112,8 +112,10 @@ class Book {
         self.pageCount = pageCount
         self.language = language
     }
+    
+    
 }
-
+//example shows that subclass only has convenience initializer
 class EnglishBook : Book {
     convenience init(id: String, pageCount: Int) {
         self.init(id: id, pageCount: pageCount, language: "English")
@@ -123,6 +125,8 @@ class EnglishBook : Book {
 let englishBook = EnglishBook(id: "123", pageCount: 232)
 
 //end
+
+
 
 class Shape {
     let id: String
@@ -145,10 +149,10 @@ class Point : Shape {
     init(id: String, x: Double, y: Double){
         self.x = x
         self.y = y
-        super.init(id: id, name: "Circle")
+        super.init(id: id, name: "Point")
     }
     
-    //Override initializers
+    //Override a convenience initializers as a convenience initializer
     convenience init(id: String) {
         self.init(id: id, x: 0.0, y: 0.0)
     }
@@ -163,5 +167,76 @@ class Circle : Shape {
     init(id: String, radius: Double) {
         self.radius = radius
         super.init(id: id, name: "Circle")
+    }
+}
+
+class Rectangle : Shape {
+    var width: Double
+    var height: Double
+    
+    //Override a convenience initializer as a designated initializer
+    init(id: String) {
+        self.width = 4
+        self.height = 5
+        super.init(id: id, name: "Rectangle")
+    }
+    
+    init(id: String, width: Double, height: Double) {
+        self.width = width
+        self.height = height
+        super.init(id: id, name: "Rectangle")
+    }
+}
+
+class Square : Rectangle {
+    
+    //Override a designated initializer as a designated initializer
+    override init(id: String, width: Double, height: Double) {
+        super.init(id: id, width: width, height: height)
+        if width != height {
+            self.width = self.height
+        }
+    }
+    
+    //Override a designated initializer as a convenience initializer
+    override convenience init(id: String) {
+        self.init(id: id, width: 4, height: 4)
+    }
+}
+
+
+class Sport {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Football : Sport {
+    init() {
+        super.init(name: "Football")
+    }
+}
+
+class Fruit {
+    
+    let name: String
+    
+    required init(name: String) {
+        self.name = name
+    }
+}
+
+class Apple : Fruit {
+    
+    var weight: Double = 0.0
+    
+    init(weight: Double) {
+        self.weight = weight
+        super.init(name: "Apple")
+    }
+    
+    required init(name: String) {
+        super.init(name: name)
     }
 }
